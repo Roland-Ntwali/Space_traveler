9109d37d044c970ead59890fb0e9bffa04fbf06e
Roland-Ntwali <rolandntwali@gmail.com>
2023-05-19 09:07:46 +0200

    Add tests for Rockets, Missions and MyProfile

---
+++ src/tests/Missions.test.js
@@ -0,0 +1,19 @@
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import Missions from '../component/Missions';
+import store from '../Redux/store';
+/* eslint-disable */
+
+jest.setTimeout(10000);
+describe("Missions", () => {
+  it("It has to match the snapshot", () => {
+    const Mission = render(
+      <>
+        <Provider store={store}>
+          <Missions />
+        </Provider>
+      </>
+    );
+    expect(Mission).toMatchSnapshot();
+  });
+});
---
+++ src/tests/Rocket.test.js
@@ -0,0 +1,18 @@
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import Rockets from '../component/Rocket';
+import store from '../Redux/store';
+/* eslint-disable */
+jest.setTimeout(10000);
+describe("Rockets", () => {
+  it("it has to match the snapshot", () => {
+    const Rocket = render(
+      <>
+        <Provider store={store}>
+          <Rockets />
+        </Provider>
+      </>
+    );
+    expect(Rocket).toMatchSnapshot();
+  });
+});
