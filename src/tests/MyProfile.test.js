5bf2e18ab9028192dc41d0be8104fc7253650da2
Roland-Ntwali <rolandntwali@gmail.com>
2023-05-19 13:37:40 +0200

    Fix linter errors

--- src/tests/MyProfile.test.js
+++ src/tests/MyProfile.test.js
@@ -2,16 +2,16 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import MyProfile from '../component/MyProfile';
 import store from '../Redux/store';
-/* eslint-disable */
+
 jest.setTimeout(10000);
-describe("My profile", () => {
-  it("it has to Render a list", () => {
+describe('My profile', () => {
+  it('it has to Render a list', () => {
     const profile = render(
       <>
         <Provider store={store}>
           <MyProfile />
         </Provider>
-      </>
+      </>,
     );
     expect(profile).toMatchSnapshot();
   });
--- src/tests/Rocket.test.js
+++ src/tests/Rocket.test.js
@@ -2,16 +2,16 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import Rockets from '../component/Rocket';
 import store from '../Redux/store';
-/* eslint-disable */
+
 jest.setTimeout(10000);
-describe("Rockets", () => {
-  it("it has to match the snapshot", () => {
+describe('Rockets', () => {
+  it('it has to match the snapshot', () => {
     const Rocket = render(
       <>
         <Provider store={store}>
           <Rockets />
         </Provider>
-      </>
+      </>,
     );
     expect(Rocket).toMatchSnapshot();
   });
